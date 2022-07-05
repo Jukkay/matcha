6b3ab3136a70b4376058b570d569462bd327d7f5
Jukka Ylimaula <jukka.ylimaula@gmail.com>
2022-07-05 21:32:42 +0300

    promisified readfile function in reset.ts

--- server/src/reset.ts
+++ server/src/reset.ts
@@ -1,28 +1,37 @@
 import mysql from 'mysql';
 import { getSecret } from "docker-secret"
-import fs from 'fs';
+import { readFile } from 'fs';
+import { promisify } from 'util';
 
 const user = process.env.MYSQL_USER
 const database = process.env.MYSQL_DATABASE
 const password = getSecret("mysql_password")
+const readFilePromise = promisify(readFile)
 
-try {
-	const db = mysql.createConnection({
-		host: 'mariadb',
-		user: user,
-		password: password,
-		database: database,
-	})
-	const reset = fs.readFileSync('config/reset.sql').toString().split(';').map(element => element.trim()).filter(element => element !== '')
-	reset.forEach((sql) =>
-		db.query(sql)
-	)
+const reset_db = async () => {
 
-	const init = fs.readFileSync('config/init.sql').toString().split(';').map(element => element.trim()).filter(element => element !== '')
-	init.forEach((sql) =>
-		db.query(sql)
-	)
-
-} catch (err) {
-	console.error(err)
+	try {
+		const db = mysql.createConnection({
+			host: 'mariadb',
+			user: user,
+			password: password,
+			database: database,
+		})
+		const reset = await readFilePromise('config/reset.sql', 'utf8')
+		const reset_queries = reset.split(';').map(element => element.trim()).filter(element => element !== '')
+		reset_queries.forEach((sql) =>
+			db.query(sql)
+		)
+		
+		const init = await readFilePromise('config/init.sql', 'utf8')
+		const init_queries = init.split(';').map(element => element.trim()).filter(element => element !== '')
+		init_queries.forEach((sql) =>
+			db.query(sql)
+		)
+		db.end()
+	} catch (err) {
+		console.error(err)
+	}
 }
+
+reset_db()
