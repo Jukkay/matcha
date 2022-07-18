e14b6dac9ce297279414aedebb78d95114bd5bf2
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-07-18 16:20:29 +0300

    fixed signup form. now displays error messages from server

---
+++ client/pages/signup/components.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { IFormInputField, IHelper, IState } from "./types";
+
+export const Label = ({ label }: IFormInputField) => {
+  if (!label) return null;
+  return <label className="label">{label}</label>;
+};
+
+export const Helper = ({ helper, focus }: IHelper) => {
+  if (!helper) return null;
+  return focus ? (
+    <p className="help">{helper}</p>
+  ) : (
+    <p className="help is-hidden">{helper}</p>
+  );
+};
+
+export const LeftIcon = ({ leftIcon }: IFormInputField) => {
+  if (!leftIcon) return null;
+  return <span className="icon is-small is-left">{leftIcon}</span>;
+};
+
+export const RightIcon = ({ rightIcon, validator }: IFormInputField) => {
+  if (!rightIcon) return null;
+  return validator ? (
+    <span className="icon is-small is-right">{rightIcon}</span>
+  ) : (
+    <span className="icon is-small is-right is-hidden">{rightIcon}</span>
+  );
+};
+
+export const ErrorMessage = ({ errorMessage, error }: IFormInputField) => {
+  if (!errorMessage) return null;
+  return error ? (
+    <p className="help is-danger">{errorMessage}</p>
+  ) : (
+    <p className="help is-danger is-hidden">{errorMessage}</p>
+  );
+};
+
+export const SubmitButton = ({ validForm }: IState) => {
+  if (validForm) return <input type="submit" className="button is-primary" />;
+  return <input type="submit" className="button is-primary" disabled />;
+};
+
+export const FormInput = ({
+  label,
+  helper,
+  errorMessage,
+  name,
+  onChange,
+  leftIcon,
+  rightIcon,
+  error,
+  validator,
+  ...inputAttributes
+}: IFormInputField) => {
+  const [helpers, setHelpers] = useState({
+    username: false,
+    password: false,
+    confirmPassword: false,
+    email: false,
+    name: false,
+  });
+
+  // Focus on field
+  const onFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHelpers({
+      ...helpers,
+      [event.target.name]: true,
+    });
+  };
+
+  // Blur field
+  const onBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHelpers({
+      ...helpers,
+      [event.target.name]: false,
+    });
+  };
+
+  return (
+    <div className="field">
+      <Label label={label} />
+      <Helper helper={helper} focus={helpers[name as keyof typeof helpers]} />
+      <div className="control has-icons-left has-icons-right">
+        <input
+          className="input"
+          name={name}
+          {...inputAttributes}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        <LeftIcon leftIcon={leftIcon} />
+        <RightIcon rightIcon={rightIcon} validator={validator} />
+      </div>
+      <ErrorMessage errorMessage={errorMessage} error={error} />
+    </div>
+  );
+};
--- client/pages/signup/index.tsx
+++ client/pages/signup/index.tsx
@@ -1,224 +1,200 @@
 import type { NextPage } from "next";
-import { useRef, useState, useEffect, ChangeEventHandler, ReactNode } from "react";
-import { IconType } from "react-icons";
-import { FaCheck, FaUser, FaLock, FaEnvelope, } from "react-icons/fa";
+import { useState, useEffect, ChangeEventHandler, ReactNode } from "react";
+import { FaCheck, FaUser, FaLock, FaEnvelope } from "react-icons/fa";
+import { SignupProps } from "./types";
+import { FormInput, SubmitButton } from "./components";
+import axios from "axios";
 
-type SignupProps = {
-  // prop types here
-};
-
-interface IFormInputField {
-  label?: string
-  helper?: string
-  errorMessage?: string
-  type?: string
-  name?: string
-  placeholder?: string
-  value?: string
-  leftIcon?: ReactNode
-  rightIcon?: ReactNode
-  onChange?: ChangeEventHandler
-  required?: boolean
-  pattern?: string
-  error?: boolean
-}
-
-const Label = ({ label }: IFormInputField) => {
-  if (!label)
-    return null
-  return (
-    <label className="label">{label}</label>
-  )
-}
-
-const Helper = ({ helper }: IFormInputField) => {
-  if (!helper)
-    return null
-  return (
-    <p className="help">{helper}</p>
-  )
-}
-
-const LeftIcon = ({ leftIcon }: IFormInputField) => {
-  if (!leftIcon)
-    return null
-  return (
-    <span className="icon is-small is-left">
-      {leftIcon}
-    </span>
-  )
-}
-const RightIcon = ({ rightIcon }: IFormInputField) => {
-  if (!rightIcon)
-    return null
-  return (
-    <span className="icon is-small is-right">
-      {rightIcon}
-    </span>
-  )
-}
-
-const ErrorMessage = ({ errorMessage, error }: IFormInputField) => {
-  if (!errorMessage)
-    return null
-  return error ? <p className="help is-danger">{errorMessage}</p> : <p className="help is-danger is-hidden">{errorMessage}</p>
-}
-
-const FormInput = ({ label, helper, errorMessage, name, onChange, leftIcon, rightIcon, error, ...inputAttributes }: IFormInputField) => {
-
-  const [hasFocus, setFocus] = useState(false);
-
-  // Focus on field
-  const onFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFocus(true)
-  }
-
-  // Blur field
-  const onBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFocus(false)
-  }
-
-  return (
-    <div className="field">
-      <Label label={label} />
-      <Helper helper={helper} />
-      <div className="control has-icons-left has-icons-right">
-        <input className="input" name={name} {...inputAttributes} onChange={onChange} onFocus={onFocus} onBlur={onBlur} />
-        <LeftIcon leftIcon={leftIcon} />
-        <RightIcon rightIcon={rightIcon} />
-      </div>
-      <ErrorMessage errorMessage={errorMessage} error={error} />
-    </div>
-  )
-}
 const Signup: NextPage = (props: SignupProps) => {
   const [validUsername, setValidUsername] = useState(false);
   const [validPassword, setValidPassword] = useState(false);
   const [validMatch, setValidMatch] = useState(false);
-
+  const [validEmail, setValidEmail] = useState(false);
+  const [validForm, setValidForm] = useState(false);
   const [values, setValues] = useState({
-    username: '',
-    password: '',
-    confirmPassword: '',
-    email: '',
-    name: ''
-  })
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+    name: "",
+  });
 
   const [errors, setErrors] = useState({
     username: false,
     password: false,
     confirmPassword: false,
     email: false,
-    name: false
-  })
+    name: false,
+  });
+
+  const [errorMessages, setErrorMessages] = useState({
+    username: "Invalid username",
+    password: "Invalid password",
+    confirmPassword: "Passwords do not match",
+    email: "Invalid email address",
+    name: "Invalid name",
+  });
 
   const inputs = [
     {
       id: 1,
-      type: 'text',
-      placeholder: 'Username',
-      name: 'username',
-      label: 'Username *',
-      helper: 'Username helper',
-      errorMessage: 'Error message',
+      type: "text",
+      placeholder: "Username",
+      name: "username",
+      label: "Username *",
+      helper:
+        "Username must be between 3 and 32 characters and may not contain special characters",
       leftIcon: <FaUser />,
       rightIcon: <FaCheck color="green" />,
-      required: true
+      validator: validUsername,
+      required: true,
     },
     {
       id: 2,
-      type: 'password',
-      placeholder: 'Password',
-      name: 'password',
-      label: 'Password *',
-      helper: 'Helper',
-      errorMessage: 'Error message',
+      type: "password",
+      placeholder: "Password",
+      name: "password",
+      label: "Password *",
+      helper:
+        "Password must be between 8 and 255 characters long and include lower (a-z) and upper (A-Z) case letters and at least one digit (0-9).",
       leftIcon: <FaLock />,
       rightIcon: <FaCheck color="green" />,
-      required: true
+      validator: validPassword,
+      required: true,
     },
     {
       id: 3,
-      type: 'password',
-      placeholder: 'Confirm password',
-      name: 'confirmPassword',
-      label: 'Confirm password *',
-      helper: 'Helper',
-      errorMessage: 'Error message',
+      type: "password",
+      placeholder: "Confirm password",
+      name: "confirmPassword",
+      label: "Confirm password *",
       leftIcon: <FaLock />,
       rightIcon: <FaCheck color="green" />,
-      required: true
+      validator: validMatch,
+      required: true,
     },
     {
       id: 4,
-      type: 'email',
-      placeholder: 'Email',
-      name: 'email',
-      label: 'Email *',
-      helper: 'Helper',
-      errorMessage: 'Error message',
+      type: "email",
+      placeholder: "Email",
+      name: "email",
+      label: "Email *",
+      helper: "Helper",
       leftIcon: <FaEnvelope />,
       rightIcon: <FaCheck color="green" />,
-      required: true
+      validator: validEmail,
+      required: true,
     },
     {
       id: 5,
-      type: 'text',
-      placeholder: 'Name',
-      name: 'name',
-      label: 'Name',
-      helper: 'Helper',
-      errorMessage: 'Error message',
+      type: "text",
+      placeholder: "Name",
+      name: "name",
+      label: "Name",
+      helper:
+        "This is the name visible for other users. If no name is given, your username is shown instead. The name must be between 3 and 32 characters and only letters and numbers are allowed.",
       leftIcon: <FaUser />,
       rightIcon: <FaCheck color="green" />,
-      required: false
+      required: false,
     },
-  ]
+  ];
 
   // Update object values
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [event.target.name]: event.target.value })
-  }
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
   // Username validation
   useEffect(() => {
-    setErrors({ ...errors, username: /^[a-zA-Z0-9.]{3,32}$/.test(values.username) });
+    if (values.username.length < 3) return;
+    const result = /^[a-zA-Z0-9.]{3,32}$/.test(values.username);
+    setErrors({
+      ...errors,
+      username: !result,
+    });
+    setErrorMessages({ ...errorMessages, username: "Invalid username" });
+    setValidUsername(result);
   }, [values.username]);
 
   // Password validation
   useEffect(() => {
-    const result = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(values.password);
-    setErrors({ ...errors, password: !result })
-    console.log(errors)
-    const match = values.password === values.confirmPassword && values.confirmPassword.length > 7;
-    setErrors({ ...errors, confirmPassword: !match })
+    if (values.password.length < 3) return;
+    const result = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]){8,255}/.test(
+      values.password
+    );
+    setErrors({ ...errors, password: !result });
+    setErrorMessages({ ...errorMessages, password: "Invalid password" });
+    console.log('setting error messages',errorMessages)
+    setValidPassword(result);
+
+    if (values.confirmPassword.length < 3) return;
+    const match = values.password === values.confirmPassword;
+    setErrors({ ...errors, confirmPassword: !match });
+    setErrorMessages({
+        ...errorMessages,
+        confirmPassword: "Passwords do not match",
+      });
+    setValidMatch(match);
   }, [values.password, values.confirmPassword]);
+
+  // Email validation
+  useEffect(() => {
+    if (values.email.length < 3) return;
+    const result = /^\S+@\S+\.\S+$/i.test(values.email);
+    setErrors({
+      ...errors,
+      email: !result,
+    });
+    setErrorMessages({
+      ...errorMessages,
+      email: "Invalid email address",
+    });
+    setValidEmail(result);
+  }, [values.email]);
 
   // Form validation
   useEffect(() => {
-    if (!(validUsername && validPassword && validMatch))
-      return
-    setValidForm(true)
-  }, [])
+    if (validUsername && validPassword && validEmail && validMatch)
+      setValidForm(true);
+    else setValidForm(false);
+  });
 
   // Handle submit
-  const handleSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault()
-  }
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4000/user/", values);
+    } catch (err: any) {
+      const errorField = err.response.data.field;
+      const errorMessage = err.response.data.message;
+      setErrors({ ...errors, [errorField]: true });
+      setErrorMessages({ ...errorMessages, [errorField]: errorMessage });
+    }
+  };
 
   return (
-
     <div className="columns">
       <div className="column is-half is-offset-one-quarter">
         <section className="section">
           <div className="box">
             <form onSubmit={handleSubmit}>
               <h3 className="title is-3">Create new account</h3>
-              {inputs.map(input => (
-                <FormInput key={input.id} {...input} error={errors[input.name as keyof typeof errors]} leftIcon={input.leftIcon} rightIcon={input.rightIcon} value={values[input.name as keyof typeof values]} onChange={onChange} />
-
+              {inputs.map((input) => (
+                <FormInput
+                  key={input.id}
+                  {...input}
+                  errorMessage={
+                    errorMessages[input.name as keyof typeof errorMessages]
+                  }
+                  error={errors[input.name as keyof typeof errors]}
+                  validator={input.validator}
+                  leftIcon={input.leftIcon}
+                  rightIcon={input.rightIcon}
+                  value={values[input.name as keyof typeof values]}
+                  onChange={onChange}
+                />
               ))}
-              <input type="submit" className="button is-primary" />
+              <SubmitButton validForm={validForm} />
             </form>
           </div>
         </section>
