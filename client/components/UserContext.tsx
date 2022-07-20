8ca3f26cf5fa32c72aa2c611d5ce7acbc9d913f5
Jukka Ylimaula <jukka.ylimaula@gmail.com>
2022-07-20 22:34:13 +0300

    tried to move the elements around. still doesn't work

--- client/components/UserContext.tsx
+++ client/components/UserContext.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, createContext } from "react";
+
 export type UserInfo = {
   username: string | undefined;
   user_id: number | null;
@@ -10,25 +10,6 @@ export type UserInfo = {
 };
 
 export interface IUser {
-  user: UserInfo;
-  updateUser: (user: UserInfo) => void;
+  user?: UserInfo;
+  updateUser?: (user: UserInfo) => void;
 }
-const [token, setToken] = useState("");
-export const [user, setUser] = useState<UserInfo>({
-	username: "",
-	user_id: null,
-	name: "",
-	email: "",
-	loggedIn: false,
-	token: token,
-	updateToken: setToken
-});
-
-// Context for user information
-export const UserContext = createContext<IUser>({
-	user: user,
-    updateUser: setUser,
-});
-
-export const useUserContext = () => useContext(UserContext);
-
--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,27 +1,39 @@
 import React, {
+  createContext,
   ReactNode,
   useEffect,
+  useState,
 } from "react";
 import Footer from "./footer";
-import {useUserContext, UserContext} from "./UserContext";
+import {UserInfo} from "./UserContext";
 import NavbarComponent from "./navbar";
 
+export const UserContext = createContext({});
 const Layout = ({ children }: { children: ReactNode }) => {
 
-  const {user, updateUser} = useUserContext()
   // Look for user information in local storage
-  useEffect(() => {
+
+    const [token, updateToken] = useState("");
+    const [user, updateUser] = useState<UserInfo>({
+      username: "",
+      user_id: null,
+      name: "",
+      email: "",
+      loggedIn: false,
+      token: token,
+      updateToken: updateToken
+    })
     const storedInfo = window.localStorage.getItem("userInfo");
     if (storedInfo) {
       const userInfo = JSON.parse(storedInfo);
       updateUser(userInfo);
       user.updateToken(userInfo.token);
     }
-  }, []);
+
   return (
     <UserContext.Provider value={{ user, updateUser }}>
       <div className="container is-widescreen">
-        <NavbarComponent loggedIn={user.loggedIn} />
+        <NavbarComponent />
         <div>
           <main>{children}</main>
         </div>
